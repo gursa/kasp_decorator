30d1529b448596abf71e083f3a4e3145a2958652
Макаров Михаил Сергеевич <m.makarov@frontol.ru>
2019-04-29 12:20:27 +0300

    Перевел тест на лямбды

--- kasp_dec/main.cpp
+++ kasp_dec/main.cpp
@@ -5,47 +5,44 @@
 #include "kasp_db_mock.h"
 #include "kasp_decorator.h"
 
-void client(kasp::decorator *db, std::size_t count, char symbol)
-{
-    for (std::size_t i = 1; i <= count; ++i)
-    {
-        std::string key(i, symbol);
-        std::string data(i, symbol+1);
-        db->put(key, data);
-        switch (i) {
-        case 100:
-        case 200:
-        case 300:
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-            break;
-        }
-    }
-}
-
-void service(kasp::decorator *db, std::size_t count, char symbol)
-{
-    for (std::size_t i = 1; i < count; ++i)
-    {
-        std::string key(i, symbol);
-        std::string data = db->get(key, std::chrono::milliseconds(300));
-        BOOST_LOG_TRIVIAL(info) << "[" << key << "," << data << "]\n";
-    }
-}
-
 int main(int argc, char* argv[])
 {
     boost::asio::io_context io;
     kasp::decorator m_db(new kasp::db_mock(),io, std::chrono::milliseconds(500));
 
-    std::vector<std::future<void>> clients;
+    auto client = [](kasp::decorator &db, std::size_t count, char symbol)
+    {
+        for (std::size_t i = 1; i <= count; ++i)
+        {
+            std::string key(i, symbol);
+            std::string data(i, symbol+1);
+            db.put(key, data);
+            if(i % 100 == 0)
+            {
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            }
+        }
+    };
+
+    auto service = [](kasp::decorator &db, std::size_t count, char symbol)
+    {
+        for (std::size_t i = 1; i < count; ++i)
+        {
+            std::string key(i, symbol);
+            std::string data = db.get(key, std::chrono::milliseconds(300));
+            BOOST_LOG_TRIVIAL(info) << "[" << key << "," << data << "]\n";
+        }
+    };
+
+    std::vector<std::future<void>> clients_services;
     for (char i = 'A'; i < 'N'; i += 2)
     {
-        clients.push_back(std::async(std::launch::async, &client, &m_db, 300, i));
+        clients_services.push_back(std::async(std::launch::async, client, std::ref(m_db), 300, i));
     }
-    std::vector<std::future<void>> services;
+
     for (char i = 'A'; i < 'D'; i += 2)
     {
-        services.push_back(std::async(std::launch::async, &service, &m_db, 300, i));
+        clients_services.push_back(std::async(std::launch::async, service, std::ref(m_db), 300, i));
     }
 
     io.run();
