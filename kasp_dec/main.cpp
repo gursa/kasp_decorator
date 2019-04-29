cd10878e84342566e8e3d30973630fd082f2070b
Макаров Михаил Сергеевич <m.makarov@frontol.ru>
2019-04-29 11:05:01 +0300

    Наводим порядок

--- kasp_dec/main.cpp
+++ kasp_dec/main.cpp
@@ -5,16 +5,15 @@
 #include "kasp_db_mock.h"
 #include "kasp_decorator.h"
 
-boost::asio::io_context io;
-kasp::decorator m_db(new kasp::db_mock(),io, std::chrono::milliseconds(500));
 
-void client(int count, char symbol)
+
+void client(kasp::decorator *db, std::size_t count, char symbol)
 {
-    for (int i = 1; i <= count; ++i)
+    for (std::size_t i = 1; i <= count; ++i)
     {
         std::string key(i, symbol);
         std::string data(i, symbol+1);
-        m_db.put(key, data);
+        db->put(key, data);
         switch (i) {
         case 100:
         case 200:
@@ -25,39 +24,32 @@ void client(int count, char symbol)
     }
 }
 
-void service(int count, char symbol)
+void service(kasp::decorator *db, std::size_t count, char symbol)
 {
-    for (int i = 1; i < count; ++i)
+    for (std::size_t i = 1; i < count; ++i)
     {
         std::string key(i, symbol);
-        std::string data = m_db.get(key, std::chrono::milliseconds(300));
+        std::string data = db->get(key, std::chrono::milliseconds(300));
         BOOST_LOG_TRIVIAL(info) << "[" << key << "," << data << "]\n";
     }
 }
 
 int main(int argc, char* argv[])
 {
+    boost::asio::io_context io;
+    kasp::decorator m_db(new kasp::db_mock(),io, std::chrono::milliseconds(500));
 
-    auto p1 = std::async(std::launch::async, &client, 300, 'A');
-    auto p2 = std::async(std::launch::async, &client, 300, 'C');
-    auto p3 = std::async(std::launch::async, &client, 300, 'E');
-    auto p4 = std::async(std::launch::async, &client, 300, 'J');
-    auto p5 = std::async(std::launch::async, &client, 300, 'K');
-    auto p6 = std::async(std::launch::async, &client, 300, 'M');
-
-
-    auto c1 = std::async(std::launch::async, &service, 300, 'A');
-    auto c2 = std::async(std::launch::async, &service, 300, 'C');
+    std::vector<std::future<void>> clients;
+    for (char i = 'A'; i < 'N'; i += 2)
+    {
+        clients.push_back(std::async(std::launch::async, &client, &m_db, 300, i));
+    }
+    std::vector<std::future<void>> services;
+    for (char i = 'A'; i < 'D'; i += 2)
+    {
+        services.push_back(std::async(std::launch::async, &service, &m_db, 300, i));
+    }
 
     io.run();
-
-    p1.get();
-    p2.get();
-    p3.get();
-    p4.get();
-    p5.get();
-    p6.get();
-    c1.get();
-    c2.get();
     return 0;
 }
